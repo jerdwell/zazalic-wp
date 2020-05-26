6cb0bdfb78448860d913786b51eec11b7590eb14
erdwell <erdwell@gmail.com>
2020-05-25 20:08:47 -0500

    ok

--- wp-content/themes/zazalic/app/src/js/navbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const navbar = document.getElementById('navbarNav')
-  const navbarButton = document.getElementById('navbarButton')
-  if(navbar == null) return
-  if(navbarButton == null) return
-  navbarButton.addEventListener('click', () => {
-    navbar.classList.toggle('collapse')
-  })
-})
